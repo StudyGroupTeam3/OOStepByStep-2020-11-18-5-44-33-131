9bd2831b37a4e0ba56160b4076df16f17bd298c3
Ruirui Huang <rhuang2@slb.com>
2020-11-18 19:59:10 +0800

    feat: add class number information to student and teacher introduce.

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -18,25 +18,25 @@ namespace OOStepByStepTest
         }
 
         [Fact]
-        public void Should_Student_Introduce_Name_Age_Job()
+        public void Should_Student_Introduce_Name_Age_Job_Class()
         {
             //given
-            Student student = new Student("Tom", 18);
+            Student student = new Student("Tom", 18, new ClassTwo());
             //when
             string actual = student.StudentIntroduce();
-            string expected = "My name is Tom. I am 18 years old. I am a student.";
+            string expected = "My name is Tom. I am 18 years old. I am a student of class 2.";
             //then
             Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void Should_Teacher_Introduce_Name_Age_Job()
+        public void Should_Teacher_Introduce_Name_Age_Job_Class()
         {
             //given
-            Teacher teacher = new Teacher("Amy", 30);
+            Teacher teacher = new Teacher("Amy", 30, new ClassTwo());
             //when
             string actual = teacher.TeacherIntroduce();
-            string expected = "My name is Amy. I am 30 years old. I am a teacher.";
+            string expected = "My name is Amy. I am 30 years old. I am a teacher of class 2.";
             //then
             Assert.Equal(expected, actual);
         }
