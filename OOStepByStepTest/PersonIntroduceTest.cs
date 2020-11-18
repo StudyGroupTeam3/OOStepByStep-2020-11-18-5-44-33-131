14b3ae42fb1a842253dbc3c7b285c0820df21023
ZFan7 <zfan7@slb.com>
2020-11-18 18:01:33 +0800

    feat: create person class that can introduce itself

---
+++ OOStepByStepTest/PersonIntroduceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OOStepByStep;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class PersonIntroduceTest
+    {
+        [Fact]
+        public void IntroduceSelf()
+        {
+            // given
+            var person = new Person("Tom", 21);
+            var expectResult = "My name is Tom. I am 21 years old.";
+
+            // when
+            var actual = person.Introduce();
+
+            // then
+            Assert.Equal(expectResult, actual);
+        }
+    }
+}
