f757e318925820ad918c0e0aba21ba75c81ac14e
Ruirui Huang <rhuang2@slb.com>
2020-11-18 19:38:38 +0800

    feat: create student and teacher class that can introduce itself.

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -12,7 +12,31 @@ namespace OOStepByStepTest
             Person person = new Person("Tom", 21);
             //when
             string actual = person.Introduce();
-            string expected = "My name is Tom. I am 21 years old";
+            string expected = "My name is Tom. I am 21 years old.";
+            //then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Student_Introduce_Name_Age_Job()
+        {
+            //given
+            Student student = new Student("Tom", 18);
+            //when
+            string actual = student.StudentIntroduce();
+            string expected = "My name is Tom. I am 18 years old. I am a student.";
+            //then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Teacher_Introduce_Name_Age_Job()
+        {
+            //given
+            Teacher teacher = new Teacher("Amy", 30);
+            //when
+            string actual = teacher.TeacherIntroduce();
+            string expected = "My name is Amy. I am 30 years old. I am a teacher.";
             //then
             Assert.Equal(expected, actual);
         }
