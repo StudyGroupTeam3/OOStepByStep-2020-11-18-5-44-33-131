48e92e0a25d8ceb19721226dac20e0bfc20e88df
Ziyuan <zxu25@slb.com>
2020-11-18 20:47:01 +0800

    feat: add Introduce() for Student and Teacher to return introduction string

---
+++ OOStepByStepTest/StudentTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOStepByStepTest
+{
+    using OOStepByStep;
+    using Xunit;
+    public class StudentTest
+    {
+        [Fact]
+        public void Should_return_student_introduction_when_introduce()
+        {
+            // given
+            Student student = new Student("Tom", 18);
+            string expect = "My name is Tom. I am 18 years old. I am a student.";
+
+            // when
+            string actual = student.Introduce();
+
+            // then
+            Assert.Equal(expect, actual);
+        }
+    }
+}
---
+++ OOStepByStepTest/TeacherTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOStepByStepTest
+{
+    using OOStepByStep;
+    using Xunit;
+    public class TeacherTest
+    {
+        [Fact]
+        public void Should_return_teacher_introduction_when_introduce()
+        {
+            // given
+            Teacher teacher = new Teacher("Amy", 30);
+            string expect = "My name is Amy. I am 30 years old. I am a teacher.";
+
+            // when
+            string actual = teacher.Introduce();
+
+            // then
+            Assert.Equal(expect, actual);
+        }
+    }
+}
