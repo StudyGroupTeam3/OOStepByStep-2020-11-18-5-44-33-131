90c6ecb8e6973b468b8c41cdf70e41dafbf5e9b0
Jiankun YANG <JYang53@slb.com>
2020-11-20 09:40:33 +0800

    feat: add source code

---
+++ OOStepByStep/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOStepByStep
+{
+    public class Teacher : Person
+    {
+        private readonly string vocation;
+        public Teacher(string name, int age) : base(name, age)
+        {
+            vocation = "teacher";
+        }
+
+        public string ClassNumber { get; set; }
+
+        public override string SelfIntroduce()
+        {
+            return $"My name is {Name}. I am {Age} years old. I am a {vocation}.";
+        }
+    }
+}
