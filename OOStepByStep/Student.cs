7c0bde5144cfc26e23024d907deb0a0b77f89269
Ruirui Huang <rhuang2@slb.com>
2020-11-24 11:22:00 +0800

    feat: push all

---
+++ OOStepByStep/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace OOStepByStep
+{
+    using System;
+    public class Student : Person
+    {
+        private readonly IClass iClass;
+        private string job = "student";
+        public Student(string name, int age, IClass iClass) : base(name, age)
+        {
+            this.iClass = iClass;
+        }
+
+        public string StudentIntroduce()
+        {
+            return this.Introduce() + $" I am a {this.job} of {this.iClass.Introduce()}.";
+        }
+    }
+}
